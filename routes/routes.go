432db2fe75f23ae31f584a08d81fc575f1701784
Felix Fong <moongod101@hotmail.com>
2020-03-19 02:02:48 +0800

    Using functions to return URL instead of map

--- routes/routes.go
+++ routes/routes.go
@@ -4,10 +4,14 @@ import "fmt"
 
 var ENDPOINT = "https://service.narvii.com/"
 
-func GetRoutes() map[string]string {
-	routes := map[string]string{
-		"Login":     fmt.Sprintf("%s/api/v1/g/s/auth/login", ENDPOINT),
-		"MyProfile": fmt.Sprintf("%s/api/v1/g/s/account", ENDPOINT),
-	}
-	return routes
+func Login() string {
+	return fmt.Sprintf("%s/api/v1/g/s/auth/login", ENDPOINT)
+}
+
+func MyProfile() string {
+	return fmt.Sprintf("%s/api/v1/g/s/account", ENDPOINT)
+}
+
+func JoinedCommunities(start int, size int) string {
+	return fmt.Sprintf("%s/api/v1/g/s/community/joined?start=%d&size=%d", ENDPOINT, start, size)
 }
