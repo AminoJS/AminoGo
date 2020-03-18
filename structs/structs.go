52b52bd7245fb7b6e7d3b6a9f8d849fc82f8ee69
Felix Fong <moongod101@hotmail.com>
2020-03-19 02:03:44 +0800

    Added a new "JoinedCommunities" structs and convert tabs into spaces

--- structs/structs.go
+++ structs/structs.go
@@ -49,3 +49,79 @@ type MyProfile struct {
 	APIDuration  string    `json:"api:duration"`
 	APITimestamp time.Time `json:"api:timestamp"`
 }
+
+type JoinedCommunities struct {
+	CommunityList []struct {
+		UserAddedTopicList interface{} `json:"userAddedTopicList"`
+		Agent              struct {
+			Status                  interface{} `json:"status"`
+			IsNicknameVerified      bool        `json:"isNicknameVerified"`
+			UID                     string      `json:"uid"`
+			Level                   int         `json:"level"`
+			FollowingStatus         int         `json:"followingStatus"`
+			AccountMembershipStatus int         `json:"accountMembershipStatus"`
+			IsGlobal                bool        `json:"isGlobal"`
+			MembershipStatus        int         `json:"membershipStatus"`
+			Reputation              int         `json:"reputation"`
+			Role                    interface{} `json:"role"`
+			NdcID                   interface{} `json:"ndcId"`
+			MembersCount            int         `json:"membersCount"`
+			Nickname                interface{} `json:"nickname"`
+			Icon                    interface{} `json:"icon"`
+		} `json:"agent"`
+		ListedStatus    int `json:"listedStatus"`
+		ProbationStatus int `json:"probationStatus"`
+		ThemePack       struct {
+			ThemeColor        string `json:"themeColor"`
+			ThemePackHash     string `json:"themePackHash"`
+			ThemePackRevision int    `json:"themePackRevision"`
+			ThemePackURL      string `json:"themePackUrl"`
+		} `json:"themePack"`
+		MembersCount    int     `json:"membersCount"`
+		PrimaryLanguage string  `json:"primaryLanguage"`
+		CommunityHeat   float64 `json:"communityHeat"`
+		StrategyInfo    string  `json:"strategyInfo"`
+		Tagline         string  `json:"tagline"`
+		JoinType        int     `json:"joinType"`
+		Status          int     `json:"status"`
+		LaunchPage      struct {
+			MediaList [][]interface{} `json:"mediaList"`
+			Title     string          `json:"title"`
+		} `json:"launchPage"`
+		ModifiedTime time.Time `json:"modifiedTime"`
+		NdcID        int       `json:"ndcId"`
+		ActiveInfo   struct {
+		} `json:"activeInfo"`
+		Link                 string          `json:"link"`
+		Icon                 string          `json:"icon"`
+		Endpoint             string          `json:"endpoint"`
+		Name                 string          `json:"name"`
+		TemplateID           int             `json:"templateId"`
+		CreatedTime          time.Time       `json:"createdTime"`
+		PromotionalMediaList [][]interface{} `json:"promotionalMediaList"`
+	} `json:"communityList"`
+	UserInfoInCommunities struct {
+		Num227549816 struct {
+			UserProfile struct {
+				Status                  int    `json:"status"`
+				IsNicknameVerified      bool   `json:"isNicknameVerified"`
+				UID                     string `json:"uid"`
+				Level                   int    `json:"level"`
+				FollowingStatus         int    `json:"followingStatus"`
+				AccountMembershipStatus int    `json:"accountMembershipStatus"`
+				IsGlobal                bool   `json:"isGlobal"`
+				MembershipStatus        int    `json:"membershipStatus"`
+				Reputation              int    `json:"reputation"`
+				Role                    int    `json:"role"`
+				NdcID                   int    `json:"ndcId"`
+				MembersCount            int    `json:"membersCount"`
+				Nickname                string `json:"nickname"`
+				Icon                    string `json:"icon"`
+			} `json:"userProfile"`
+		} `json:"227549816"`
+	} `json:"userInfoInCommunities"`
+	APIMessage    string    `json:"api:message"`
+	APIStatuscode int       `json:"api:statuscode"`
+	APIDuration   string    `json:"api:duration"`
+	APITimestamp  time.Time `json:"api:timestamp"`
+}
