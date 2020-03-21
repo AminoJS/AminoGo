4eea1eacbf1d4528a75a806f243a35706d508a2d
Felix Fong <moongod101@hotmail.com>
2020-03-21 16:23:29 +0800

    Example code for the new "GetBlogFeed" function

---
+++ examples/GetCommunityBlogs.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"github.com/AminoJS/AminoGo/aminogo"
+	"os"
+)
+
+func main() {
+	err := aminogo.Login(os.Getenv("AMINO_USERNAME"), os.Getenv("AMINO_PASSWORD"))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	yourFirstCommunity, err := aminogo.GetJoinedCommunities(aminogo.QueryOptions{
+		Start: 0,
+		Size:  1,
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	blogs, err := aminogo.GetBlogFeed(yourFirstCommunity.CommunityList[0].NdcID, 0, 1)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	blog := blogs.BlogList[0]
+	fmt.Printf("Blog ID: %s\nContent:\n", blog.Content)
+}
