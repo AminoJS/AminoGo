e01bc325fa940d1ff58ff4c2e8171998890241a5
Felix Fong <moongod101@hotmail.com>
2020-03-22 18:26:23 +0800

    Using the renamed options name and using the new pointer reference instead

--- examples/GetCommunityBlogs.go
+++ examples/GetCommunityBlogs.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-    "fmt"
-    "github.com/AminoJS/AminoGo/aminogo"
-    "os"
+	"fmt"
+	"github.com/AminoJS/AminoGo/aminogo"
+	"os"
 )
 
 func main() {
-    err := aminogo.Login(os.Getenv("AMINO_USERNAME"), os.Getenv("AMINO_PASSWORD"))
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    
-    yourFirstCommunity, err := aminogo.GetJoinedCommunities(aminogo.QueryOptions{
-        Start: 0,
-        Size:  1,
-    })
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    
-    blogs, err := aminogo.GetBlogFeed(yourFirstCommunity.CommunityList[0].NdcID, 0, 1)
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    blog := blogs.BlogList[0]
-    fmt.Printf("Blog ID: %d\nContent:\n%s", blog.NdcID, blog.Content)
+	err := aminogo.Login(os.Getenv("AMINO_USERNAME"), os.Getenv("AMINO_PASSWORD"))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	yourFirstCommunity, err := aminogo.GetJoinedCommunities(&aminogo.GetJoinedCommunitiesOptions{
+		Start: 0,
+		Size:  1,
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	blogs, err := aminogo.GetBlogFeed(yourFirstCommunity.CommunityList[0].NdcID, 0, 1)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	blog := blogs.BlogList[0]
+	fmt.Printf("Blog ID: %d\nContent:\n%s", blog.NdcID, blog.Content)
 }
