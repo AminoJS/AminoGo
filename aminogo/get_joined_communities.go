c8e076fa234c167de2a06616734393c9a1fad304
Felix Fong <moongod101@hotmail.com>
2020-03-22 18:25:10 +0800

    Rename option struct

--- aminogo/get_joined_communities.go
+++ aminogo/get_joined_communities.go
@@ -1,48 +1,48 @@
 package aminogo
 
 import (
-    "errors"
-    "fmt"
-    "github.com/AminoJS/AminoGo/utils"
-    
-    "github.com/AminoJS/AminoGo/routes"
-    "github.com/AminoJS/AminoGo/stores"
-    "github.com/AminoJS/AminoGo/structs"
-    "github.com/imroc/req"
+	"errors"
+	"fmt"
+	"github.com/AminoJS/AminoGo/utils"
+
+	"github.com/AminoJS/AminoGo/routes"
+	"github.com/AminoJS/AminoGo/stores"
+	"github.com/AminoJS/AminoGo/structs"
+	"github.com/imroc/req"
 )
 
-type QueryOptions struct {
-    Start int
-    Size  int
+type GetJoinedCommunitiesOptions struct {
+	Start int
+	Size  int
 }
 
-func GetJoinedCommunities(argument QueryOptions) (joinedCommunities *structs.JoinedCommunities, err error) {
-    SID := stores.Get("SID")
-    if SID == nil {
-        return &structs.JoinedCommunities{}, errors.New("missing SID in state, try using aminogo.Login() first")
-    }
-    
-    // Send the actual request to the Amino API endpoint
-    
-    header := req.Header{
-        "NDCAUTH": fmt.Sprintf("sid=%s", SID),
-    }
-    
-    endpoint := routes.JoinedCommunities(argument.Start, argument.Size)
-    
-    utils.DebugLog("get_joined_communities.go", fmt.Sprintf("URL: %s", endpoint))
-    
-    res, err := req.Get(endpoint, header)
-    if err != nil {
-        return &structs.JoinedCommunities{}, nil
-    }
-    
-    resMap := structs.JoinedCommunities{}
-    err = res.ToJSON(&resMap)
-    if err != nil {
-        return &structs.JoinedCommunities{}, err
-    }
-    
-    return &resMap, nil
-    
+func GetJoinedCommunities(argument *GetJoinedCommunitiesOptions) (joinedCommunities *structs.JoinedCommunities, err error) {
+	SID := stores.Get("SID")
+	if SID == nil {
+		return &structs.JoinedCommunities{}, errors.New("missing SID in state, try using aminogo.Login() first")
+	}
+
+	// Send the actual request to the Amino API endpoint
+
+	header := req.Header{
+		"NDCAUTH": fmt.Sprintf("sid=%s", SID),
+	}
+
+	endpoint := routes.JoinedCommunities(argument.Start, argument.Size)
+
+	utils.DebugLog("get_joined_communities.go", fmt.Sprintf("URL: %s", endpoint))
+
+	res, err := req.Get(endpoint, header)
+	if err != nil {
+		return &structs.JoinedCommunities{}, err
+	}
+
+	resMap := structs.JoinedCommunities{}
+	err = res.ToJSON(&resMap)
+	if err != nil {
+		return &structs.JoinedCommunities{}, err
+	}
+
+	return &resMap, nil
+
 }
