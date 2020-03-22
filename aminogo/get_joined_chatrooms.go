2d1bd0cc7a7905b32a382ec3c25653bcbbe52e9c
Felix Fong <moongod101@hotmail.com>
2020-03-22 18:23:25 +0800

    New function for getting a list of joined chat rooms from a targeted community

---
+++ aminogo/get_joined_chatrooms.go
@@ -0,0 +1,48 @@
+package aminogo
+
+import (
+	"errors"
+	"fmt"
+	"github.com/AminoJS/AminoGo/routes"
+	"github.com/AminoJS/AminoGo/stores"
+	"github.com/AminoJS/AminoGo/structs"
+	"github.com/AminoJS/AminoGo/utils"
+	"github.com/imroc/req"
+)
+
+type GetJoinedChatroomsOptions struct {
+	CommunityID int
+	Start       int
+	Size        int
+}
+
+func GetJoinedChatrooms(argument *GetJoinedChatroomsOptions) (joinedChatrooms *structs.JoinedChatrooms, err error) {
+	SID := stores.Get("SID")
+	if SID == nil {
+		return &structs.JoinedChatrooms{}, errors.New("missing SID in state, try using aminogo.Login() first")
+	}
+
+	header := req.Header{
+		"NDCAUTH": fmt.Sprintf("sid=%s", SID),
+	}
+
+	endpoint := routes.GetJoinedChatrooms(argument.CommunityID, argument.Start, argument.Size)
+
+	utils.DebugLog("get_joined_chatrooms.go", fmt.Sprintf("URL: %s", endpoint))
+
+	res, err := req.Get(endpoint, header)
+	if err != nil {
+		return &structs.JoinedChatrooms{}, err
+	}
+
+	//fmt.Println(res.ToString())
+
+	resMap := structs.JoinedChatrooms{}
+	err = res.ToJSON(&resMap)
+	if err != nil {
+		return &structs.JoinedChatrooms{}, err
+	}
+
+	return &resMap, nil
+
+}
