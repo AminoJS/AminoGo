251811bf60f8e10935b0ec0b3b9922032a7b1a72
Felix Fong <moongod101@hotmail.com>
2020-03-22 18:26:51 +0800

    Using pointer struct

--- examples/GetJoinedCommunities.go
+++ examples/GetJoinedCommunities.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    
-    "github.com/AminoJS/AminoGo/aminogo"
+	"fmt"
+	"os"
+
+	"github.com/AminoJS/AminoGo/aminogo"
 )
 
 func main() {
-    err := aminogo.Login(os.Getenv("AMINO_USERNAME"), os.Getenv("AMINO_PASSWORD"))
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    joinedCommunities, err := aminogo.GetJoinedCommunities(aminogo.QueryOptions{
-        Start: 0,
-        Size:  100,
-    })
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    firstCommunity := joinedCommunities.CommunityList[0]
-    fmt.Printf("First Community Icon: %s\n", firstCommunity.NdcID)
+	err := aminogo.Login(os.Getenv("AMINO_USERNAME"), os.Getenv("AMINO_PASSWORD"))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	joinedCommunities, err := aminogo.GetJoinedCommunities(&aminogo.GetJoinedCommunitiesOptions{
+		Start: 0,
+		Size:  100,
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	firstCommunity := joinedCommunities.CommunityList[0]
+	fmt.Printf("First Community Icon: %s\n", firstCommunity.NdcID)
 }
