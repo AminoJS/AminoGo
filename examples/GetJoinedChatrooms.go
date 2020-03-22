ebc23a83268e6ad0e30c499cf66936e310d35434
Felix Fong <moongod101@hotmail.com>
2020-03-22 18:26:42 +0800

    Example code for the new GetJoinedChatrooms.go module

---
+++ examples/GetJoinedChatrooms.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"github.com/AminoJS/AminoGo/aminogo"
+	"os"
+)
+
+func main() {
+	err := aminogo.Login(os.Getenv("AMINO_USERNAME"), os.Getenv("AMINO_PASSWORD"))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	yourFirstCommunity, err := aminogo.GetJoinedCommunities(&aminogo.GetJoinedCommunitiesOptions{
+		Start: 0,
+		Size:  1,
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	chatrooms, err := aminogo.GetJoinedChatrooms(&aminogo.GetJoinedChatroomsOptions{
+		CommunityID: yourFirstCommunity.CommunityList[0].NdcID,
+		Start:       0,
+		Size:        1,
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	chatroom := chatrooms.ThreadList[0]
+	fmt.Printf("Chatroom Title: %s\nFrom Community: %s\n", chatroom.Title, yourFirstCommunity.CommunityList[0].Name)
+}
