af3e3857df3c364043ddc89f5890e269704d6a22
Felix Fong <moongod101@hotmail.com>
2020-03-27 00:03:41 +0800

    Added unit test case for get_joined_chatrooms.go

---
+++ test/get_joined_chatrooms_test.go
@@ -0,0 +1,63 @@
+package test
+
+import (
+	"errors"
+	"github.com/AminoJS/AminoGo/aminogo"
+	"github.com/AminoJS/AminoGo/test_utils"
+	"os"
+	"testing"
+)
+
+func TestRequestJoinedChatroomsforeLogin(t *testing.T) {
+	_, err := aminogo.GetJoinedChatrooms(&aminogo.GetJoinedChatroomsOptions{
+		CommunityID: 0,
+		Start:       0,
+		Size:        0,
+	})
+	if err == nil {
+		t.Error("There should be an error since we have obtain a session token yet")
+	}
+}
+
+func TestInvalidCommunityIdForChatrooms(t *testing.T) {
+	if err := aminogo.Login(os.Getenv("AMINO_USERNAME"), os.Getenv("AMINO_PASSWORD")); err != nil {
+		t.Error(err)
+	}
+	_, err := aminogo.GetJoinedChatrooms(&aminogo.GetJoinedChatroomsOptions{
+		CommunityID: 0,
+		Start:       0,
+		Size:        0,
+	})
+	if err == nil {
+		t.Error("There should be an error since we have obtain a session token yet")
+	}
+	expectedErr := errors.New("fail to login API call due to bad request (perhaps you are giving the wrong arguments), resulted in a none 400 status code")
+	test_utils.ExpectError(expectedErr, err, t)
+}
+
+func TestGetJoinedChatrooms(t *testing.T) {
+	if err := aminogo.Login(os.Getenv("AMINO_USERNAME"), os.Getenv("AMINO_PASSWORD")); err != nil {
+		t.Error(err)
+	}
+
+	myCom, err := aminogo.GetJoinedCommunities(&aminogo.GetJoinedCommunitiesOptions{
+		Start: 0,
+		Size:  1,
+	})
+	if err != nil {
+		t.Error(err)
+	}
+
+	chatroms, err := aminogo.GetJoinedChatrooms(&aminogo.GetJoinedChatroomsOptions{
+		CommunityID: myCom.CommunityList[0].NdcID,
+		Start:       0,
+		Size:        0,
+	})
+	if err != nil {
+		t.Error(err)
+	}
+
+	if len(chatroms.ThreadList) <= 0 {
+		t.Error("Length of chatrooms list shouldn't result in less then or equal 0 since this test Amino has indeed joined a chat room")
+	}
+}
