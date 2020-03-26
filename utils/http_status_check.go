c7c5aa94150b414b60643e6c74e2b993f0a1139e
Felix Fong <moongod101@hotmail.com>
2020-03-27 06:02:57 +0800

    Modify the err msg before sent; Added a new "Amino Said" field for showing "api:message" from raw response from Amino's API

--- utils/http_status_check.go
+++ utils/http_status_check.go
@@ -1,31 +1,55 @@
 package utils
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
-// Return a error message for some common HTTP error with some tips
-func ThrowHttpErrorIfFail(statusCode int) (err error) {
+func formReturnMessage(msg string, aminoMsg interface{}) error {
+	msg += fmt.Sprintf("\nAmino Said: %v", aminoMsg)
+	return errors.New(msg)
+}
 
-	if statusCode == 0 {
+// Return a error message for some common HTTP error with some tips
+func ThrowHttpErrorIfFail(res *http.Response) (err error) {
+
+	if res.StatusCode == 0 {
 		return errors.New("empty or 0 HTTP status is not allowed")
 	}
 
-	switch statusCode {
-	case 404:
-		return errors.New("fail to login API call due to resource not found, resulted in a none 404 status code")
-	case 400:
-		return errors.New("fail to login API call due to bad request (perhaps you are giving the wrong arguments), resulted in a none 400 status code")
-	case 401:
-		return errors.New("fail to login API call due to unauthorized (perhaps you could try re-login thus generating a new session-token), resulted in a none 400 status code")
-	case 405:
-		return errors.New("fail to login API call due to bad method not allowed (there must be a bug in AminoGo, please ensure AminoGo are update-to-date, if you are on the latest version, please repo this on GitHub at https://github.com/AminoJS/AminoGo/issues), resulted in a none 400 status code")
-	case 500:
-		return errors.New("fail to login API call due to internal server error(not your fault), resulted in a none 500 status code")
+	var aminoMsg interface{}
+
+	if res.Body != nil {
+		var body map[string]interface{}
+
+		jStr, _ := ioutil.ReadAll(res.Body)
+
+		if string(jStr) != "" {
+			json.Unmarshal(jStr, body)
+
+			aminoMsg = body["api:message"].(interface{})
+		} else {
+			aminoMsg = "(AminoGo Could Not Parse Error Message)"
+		}
 	}
-	if statusCode != 200 && statusCode != 201 {
-		return errors.New(fmt.Sprintf("fail to login API call, resulted in a none %d status code", statusCode))
+
+	switch res.StatusCode {
+	case 404:
+		return formReturnMessage("fail to login API call due to resource not found, resulted in a none 404 status code", aminoMsg)
+	case 400:
+		return formReturnMessage("fail to login API call due to bad request (perhaps you are giving the wrong arguments), resulted in a none 400 status code", aminoMsg)
+	case 401:
+		return formReturnMessage("fail to login API call due to unauthorized (perhaps you could try re-login thus generating a new session-token), resulted in a none 400 status code", aminoMsg)
+	case 405:
+		return formReturnMessage("fail to login API call due to bad method not allowed (there must be a bug in AminoGo, please ensure AminoGo are update-to-date, if you are on the latest version, please repo this on GitHub at https://github.com/AminoJS/AminoGo/issues), resulted in a none 400 status code", aminoMsg)
+	case 500:
+		return formReturnMessage("fail to login API call due to internal server error(not your fault), resulted in a none 500 status code", aminoMsg)
+	}
+	if res.StatusCode != 200 && res.StatusCode != 201 {
+		return formReturnMessage(fmt.Sprintf("fail to login API call, resulted in a none %d status code", res.StatusCode), aminoMsg)
 	}
 	return nil
 }
