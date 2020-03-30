9ec1bdab8b325c1e7286ffd1915f168c4d46e3fd
Felix Fong <moongod101@hotmail.com>
2020-03-30 23:08:58 +0800

    Add new function for posting JSON

--- utils/http_req.go
+++ utils/http_req.go
@@ -24,3 +24,21 @@ func Get(url string) (*req.Resp, error) {
 	res, err := req.Get(url, header)
 	return res, err
 }
+
+func PostJSON(url string, data interface{}) (*req.Resp, error) {
+	if url == "" {
+		return &req.Resp{}, errors.New("URL cannot be emtpy")
+	}
+	SID := stores.Get("SID")
+	if SID == nil || SID == "" {
+		return &req.Resp{}, errors.New("missing SID in state, try using aminogo.Login() first")
+	}
+	header := req.Header{
+		"NDCAUTH": fmt.Sprintf("sid=%s", SID),
+		//"Content-Type": "application/json",
+	}
+
+	req.SetTimeout(30 * time.Second)
+	res, err := req.Post(url, header, req.BodyJSON(data))
+	return res, err
+}
